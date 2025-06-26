a40636a6f02135876e6b9e81f6993e924e530388
Dominic Lingham <dominic.lingham@educationcompany.co.uk>
2025-06-26 21:23:55 +0100

    feat: add base layout and simple navbar

---
+++ components/app/NavBar.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <nav>
+    <UContainer>
+      <div class="flex justify-between items-center py-4">
+        <NuxtLink
+          to="/"
+          class="text-xl font-bold flex items-center md:text-3xl "
+        >
+          <img
+            src="/SyntaxStashLogo-dark.svg"
+            height="50px"
+            width="70px"
+          >
+          <span>SyntaxStash</span>
+        </NuxtLink>
+
+        <UButton>Login</UButton>
+      </div>
+    </UContainer>
+  </nav>
+</template>
+
+<style>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="flex min-h-screen flex-col">
+    <AppNavBar />
+    <main>
+      <UContainer>
+        <slot />
+      </UContainer>
+    </main>
+  </div>
+</template>
+
+<style>
+
+</style>
