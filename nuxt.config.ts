89510f12980da44fe44068d7c27c27ce4f8a3cc8
Dominic Lingham <dominic.lingham@educationcompany.co.uk>
2025-06-26 20:37:10 +0100

    feat: add tailwind and nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/ui',
+  ],
+  css: ['~/assets/css/main.css'],
   eslint: {
     config: {
       standalone: false,
