6d30328098988244d2888adbaf485862442d13ce
Dominic Lingham <103593100+DominicLingham@users.noreply.github.com>
2025-07-01 16:56:31 +0100

    feat: nav changes and theme toggle (#5)

--- components/app/NavBar.vue
+++ components/app/NavBar.vue
@@ -1,24 +1,29 @@
 <script lang="ts" setup>
-
+import ThemeToggle from './ThemeToggle.vue'
 </script>
 
 <template>
   <nav>
     <UContainer>
       <div class="flex justify-between items-center py-4">
-        <NuxtLink
-          to="/"
-          class="text-xl font-bold flex items-center md:text-3xl "
-        >
-          <img
-            src="/SyntaxStashLogo-dark.svg"
-            height="50px"
-            width="70px"
+        <div>
+          <NuxtLink
+            to="/"
+            class="text-xl font-bold flex items-center md:text-3xl "
           >
-          <span>SyntaxStash</span>
-        </NuxtLink>
+            <img
+              src="/SyntaxStashLogo-dark.svg"
+              height="50px"
+              width="70px"
+            >
+            <span>SyntaxStash</span>
+          </NuxtLink>
+        </div>
 
-        <UButton>Login</UButton>
+        <div class="flex items-center gap-4">
+          <ThemeToggle />
+          <UButton>Get started</UButton>
+        </div>
       </div>
     </UContainer>
   </nav>
