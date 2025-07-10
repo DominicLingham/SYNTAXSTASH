bdf6649efd28c7bc5ae7fc42cb96fd4c1ce73cb4
Dominic Lingham <dominic.lingham@educationcompany.co.uk>
2025-07-10 12:49:46 +0100

    feat: add user info on login and add logout functionality

---
+++ components/auth/UserDropdown.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+import type { DropdownMenuItem } from '@nuxt/ui'
+
+const authStore = useAuthStore()
+
+const router = useRouter()
+
+const items = ref<DropdownMenuItem[]>([
+  {
+    label: 'Logout',
+    icon: 'i-lucide-log-out',
+    type: 'link',
+    onSelect: () => router.push('/sign-out'),
+  },
+])
+</script>
+
+<template>
+  <UDropdownMenu
+    v-if="authStore.user"
+    :items="items"
+    :content="{
+      align: 'end',
+      side: 'bottom',
+      sideOffset: 8,
+    }"
+    :ui="{
+      content: 'w-48',
+    }"
+    color="primary"
+  >
+    <UButton
+      variant="link"
+      size="xl"
+      :label="authStore.user.name"
+      :avatar="{
+        src: authStore.user.image,
+      }"
+    />
+  </UDropdownMenu>
+</template>
+
+<style>
+
+</style>
