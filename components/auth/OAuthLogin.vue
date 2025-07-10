f4edb62a0cf44d94ec229d10adcb43061e86b610
Dominic Lingham <103593100+DominicLingham@users.noreply.github.com>
2025-07-10 10:27:56 +0100

    feat: add auth store and generic error page (#16)

--- components/auth/OAuthLogin.vue
+++ components/auth/OAuthLogin.vue
@@ -1,18 +1,5 @@
 <script lang="ts" setup>
-import { createAuthClient } from 'better-auth/client'
-
-const authClient = createAuthClient()
-
-const loading = ref(false)
-
-async function signIn() {
-  loading.value = true
-  await authClient.signIn.social({
-    provider: 'github',
-    callbackURL: '/dashboard',
-  })
-  loading.value = false
-}
+const authStore = useAuthStore()
 </script>
 
 <template>
@@ -20,11 +7,11 @@ async function signIn() {
     <UButton
       icon="i-mdi:github"
       block
-      :disabled="loading"
-      :loading="loading"
-      @click="signIn"
+      :disabled="authStore.loading"
+      :loading="authStore.loading"
+      @click="authStore.signInGithub"
     >
-      Log in with GitHub
+      Login with GitHub
     </UButton>
   </div>
 </template>
