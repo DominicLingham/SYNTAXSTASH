4ec1078b777c976786642bd2332fdcf645a73665
Dominic Lingham <dominic.lingham@educationcompany.co.uk>
2025-07-26 23:38:05 +0100

    feat: add custom tiptap editor

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,16 +1,9 @@
 <script lang="ts" setup>
-import TipTapEditor from '~/components/app/TipTapEditor.vue'
-
-const content = ref()
 </script>
 
 <template>
   <div>
     <h1>You are logged in</h1>
-    <UContainer class="mt-4">
-      <TipTapEditor v-model="content" />
-    </UContainer>
-    <div />
   </div>
 </template>
 
