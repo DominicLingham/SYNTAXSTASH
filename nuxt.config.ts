7d93a75e6cc98b80a9b115bbdbd1275b52a73f1d
Dominic Lingham <103593100+DominicLingham@users.noreply.github.com>
2025-07-31 10:21:17 +0100

    Fix/remove vee validate (#27)
    * fix: remove-vee-validate
    
    * fix: reinstall node packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,6 @@ export default defineNuxtConfig({
     '@nuxtjs/color-mode',
     '@vueuse/nuxt',
     '@pinia/nuxt',
-    '@vee-validate/nuxt',
   ],
   css: ['~/assets/css/main.css'],
   eslint: {
